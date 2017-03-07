f29352d9c83366df8ea82049cc9be7699c5f8801
phist210 <josh.friese@gmail.com>
2017-03-06 23:38:43 -0500

    latest commit, tests on the way

--- blackjack.py
+++ blackjack.py
@@ -9,13 +9,9 @@ RANK = 'A23456789TJQK'
 DECK = tuple(''.join(card) for card in product(RANK, SUIT))
 """Creates the deck (a set) by joining all possibilities of RANK, SUIT pairs.
 """
-DECKVAL = {card: min(1 + RANK.index(card[0]), 10) for card in DECK}
+DECKVALUES = {card: min(1 + RANK.index(card[0]), 10) for card in DECK}
 """Creates a dictionary of "Cards" as keys and their respective values as values
 """
-
-
-def clear():
-    os.system('clear')
 
 
 class OutOfCards(Exception):
@@ -25,15 +21,15 @@ class OutOfCards(Exception):
 class Deck:
     """A shuffled deck of cards."""
     def __init__(self):
-        self.cards = random.sample(DECK, len(DECK))
+        self.game_deck = random.sample(DECK, len(DECK))
 
-    def draw(self):
+    def first_draw(self):
         """Draw one card from the deck and return it.
         Raise OutOfCards if the deck is out of cards.
 
         """
-        if self.cards:
-            return self.cards.pop()
+        if self.game_deck:
+            return self.game_deck.pop()
         else:
             raise OutOfCards()
 
@@ -41,23 +37,23 @@ class Deck:
 class Player:
     """A hand of cards in blackjack."""
     def __init__(self, DECK):
-        self.cards = []
+        self.game_deck = []
         for i in range(2):
             self.draw(DECK)
 
     def __str__(self):
-        return ' '.join(self.cards)
+        return ' '.join(self.game_deck)
 
-    def draw(self, deck):
+    def draw(self, game_deck):
         """Draw one card from deck. Return the new card."""
-        card = deck.draw()
-        self.cards.append(card)
+        card = game_deck.first_draw()
+        self.game_deck.append(card)
         return card
 
     @property
     def soft_value(self):
         """The "soft" value of the cards in the hand (counting aces as 1)."""
-        return sum(DECKVAL[card] for card in self.cards)
+        return sum(DECKVALUES[card] for card in self.game_deck)
 
     @property
     def value(self):
@@ -65,11 +61,14 @@ class Player:
         unless this would go bust, otherwise as 1).
 
         """
-        v = self.soft_value
-        if v <= 11 and any(card[0] == 'A' for card in self.cards):
-            return v + 10
+        value = self.soft_value
+        if value <= 11 and any(card[0] == 'A' for card in self.game_deck):
+            """Check if card's value is less than 11 and if it's an Ace to determine
+                hard hand.
+            """
+            return value + 10
         else:
-            return v
+            return value
 
     @property
     def bust(self):
@@ -79,7 +78,16 @@ class Player:
     @property
     def blackjack(self):
         """True if this hand is a blackjack (ace plus ten or face card)."""
-        return len(self.cards) == 2 and self.value == 21
+        return len(self.game_deck) == 2 and self.value == 21
+
+
+# ===================================================
+#                       GAME
+# ===================================================
+
+
+def clear():
+    os.system('clear')
 
 
 def play_again():
@@ -97,15 +105,16 @@ def play_again():
 def main():
     """Play a hand of blackjack."""
 
-    deck = Deck()
-    hand = Player(deck)
-    dealer = Player(deck)
+    deck_this_round = Deck()
+    hand = Player(deck_this_round)
+    dealer = Player(deck_this_round)
 
     clear()
+    # Welcome screen
     print("========\/\/\/\/\/ Welcome to 21 Pythonjack! \/\/\/\/\/========\n")
     print("Suits:  [S] = Spades | [C] = Clubs | [D] = Diamonds | [H] = Hearts")
     print("Values:  [A] = Ace(1) | [T] = 10 | [J] = Jack | [Q] = Queen | [K] = King\n")
-    print("Dealer's hand: {}".format(dealer.cards[0]))
+    print("Dealer's hand: {}".format(dealer.game_deck[0]))
 
     # Player logic
     while True:
@@ -119,7 +128,7 @@ def main():
         if choice == 's':
             break
         elif choice == 'h':
-            card = hand.draw(deck)
+            card = hand.draw(deck_this_round)
             print("\nYou drew: {}".format(card))
         else:
             print("Invalid input. Try again!\n")
@@ -129,24 +138,29 @@ def main():
         print("Dealer's hand: {} | Total = {}\n".format(dealer, dealer.value))
         if dealer.soft_value >= 17:
             break
-        card = dealer.draw(deck)
+        card = dealer.draw(deck_this_round)
         print("Dealer drew: {}\n".format(card))
 
     if dealer.bust:
         print("Dealer is bust. You WIN!\n")
         play_again()
+
     elif dealer.blackjack and not hand.blackjack:
         print("Dealer's blackjack beats your {}\n".format(hand.value))
         play_again()
+
     elif hand.blackjack and not dealer.blackjack:
         print("Your blackjack beats dealer's {}!\n".format(dealer.value))
         play_again()
+
     elif dealer.value > hand.value:
         print("Dealer's {} beats your {}!\n".format(dealer.value, hand.value))
         play_again()
+
     elif hand.value > dealer.value:
         print("Your {} beats dealer's {}!\n".format(hand.value, dealer.value))
         play_again()
+
     else:
         print("Push: {} each\n".format(hand.value))
         play_again()
